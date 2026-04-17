6c6ea5d3f2cf41e23fca1897decf7073501f9fe2
chisengab146-cpu <lumbwahabibi472@gmailMULUNGUSHI>
2026-04-17 20:25:44 +0200

    My Second Work

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables{
+    public static void main(String[] args){
+        String name = "Brian Bupe Chisenga";
+        int age = 19;
+        String course = "Computer Science";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(course);
+    }
+}
